231fd5b7e34263904d3aede0107220bf89c2b1f9
Huang Jian-Min <yfr.huang@hotmail.com>
2019-01-24 14:45:06 +0800

    add sample code

---
+++ mafia-backend-mono/src/main/kotlin/org/common/marvel/mafia/service/impl/ConnectorServiceImpl.kt
@@ -0,0 +1,18 @@
+package org.common.marvel.mafia.service.impl
+
+import com.corundumstudio.socketio.SocketIOServer
+import org.common.marvel.mafia.service.ConnectorService
+import org.springframework.stereotype.Service
+import javax.annotation.Resource
+
+@Service
+class ConnectorServiceImpl : ConnectorService {
+
+    @Resource
+    lateinit var socketIOServer : SocketIOServer
+
+    override fun start() {
+        socketIOServer.start()
+    }
+
+}
